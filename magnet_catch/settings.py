3bc109d2faaadf8fab834f1b75553160518d397b
Bealby <mgbealby@gmail.com>
2021-03-01 18:47:43 +0000

    Set-up background image for body

--- magnet_catch/settings.py
+++ magnet_catch/settings.py
@@ -148,6 +148,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
